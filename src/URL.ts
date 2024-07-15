6be3e47b9fc93ea0e27fed3e236fd4c13ed0a599
Dan-tk <thiongodan405@gmail.com>
2024-07-15 04:13:53 +0300

    other changes

--- src/URL.ts
+++ src/URL.ts
@@ -1,2 +1,2 @@
-/* export const URL = "http://localhost:8000" */
-export const URL = "https://gr-8-api.onrender.com"
+ export const URL = "http://localhost:8000" 
+/* export const URL = "https://gr-8-api.onrender.com" */
